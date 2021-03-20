6f0b04747234ec4b0820ef83f3885b1a84272751
darthshittious <varunj26012001@gmail.com>
2021-03-20 23:54:39 +0530

    Windows sucks

--- main.sh
+++ main.sh
@@ -1,9 +1,13 @@
 sudo apt update && sudo apt upgrade -y
-postgres.sh
-python.sh
-rust.sh
-useful.sh
-web.sh
+curdir="$(dirname "$0")"
+olddir="$(pwd)"
+cd "$curdir" || exit
+./postgres.sh
+./python.sh
+./rust.sh
+./useful.sh
+./web.sh
+cd "$olddir" || exit
 sudo apt install language-pack-en
 export LC_ALL="en_GB.UTF-8"
 sudo dpkg-reconfigure locales
